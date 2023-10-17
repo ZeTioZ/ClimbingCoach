a8205b960586a7849c7b946523e519304d53b1f3
ZeTioZ <donato_gentile@hotmail.it>
2023-10-17 23:04:38 +0200

    Converting from detectron2 to YoloV8

--- src/libs/model_loader.py
+++ src/libs/model_loader.py
@@ -1,36 +1,11 @@
-import numpy as np
-
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
+from ultralytics import YOLO
 
 
-"""
-Loads a model from a file and predicts objects in images.
-"""
 class ModelLoader:
-    def __init__(self, merge_from_file, model, model_device: str = 'cuda', score_thresh_test: float = 0.8):
-        """
-        Initializes the ModelLoader.
+    def __init__(self, model_path):
+        self.model = YOLO(model_path)
 
-        :param merge_from_file: The path to the config file to merge with the default config.
-        :param model: The path to the model file.
-        :param model_device: The device to use for the model.
-        :param score_thresh_test: The score threshold for the model.
-        """
-        self.cfg = get_cfg()
-        self.cfg.merge_from_file(merge_from_file)
-        self.cfg.MODEL.WEIGHTS = model
-        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = score_thresh_test
-        self.cfg.MODEL.DEVICE=model_device
-        self.predictor = DefaultPredictor(self.cfg)
 
-    
-    def predict(self, image: np.ndarray) -> dict:
-        """
-        Predicts objects in the given image.
-
-        :param image: The image to predict objects in.
-        :return: A dictionary containing the predictions.
-        """
-        outputs = self.predictor(image)
-        return outputs
+    def predict(self, image):
+        predictions = self.model(image)
+        return predictions
--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
 import cv2, os
 
 from libs.model_loader import ModelLoader
-from utils.detectron2_converter_utils import convert_image_box_output
-from utils.draw_utils import skeleton_visualizer, box_visualizer, refresh
+from utils.yolov8_converter_utils import convert_image_box_outputs, convert_image_skeleton_outputs
+from utils.draw_utils import skeleton_visualizer, box_visualizer
 
-from utils.skeleton_scanner import SkeletonScanner
 from objects.skeleton import Skeleton
-from detectron2 import model_zoo
 
 MODELS_DIRECTORY = "./resources/models/"
 VIDEOS_DIRECTORY = "./resources/videos/"
@@ -21,68 +19,50 @@ def test_holds_detector():
     """
     Tests the HoldsDetector class by detecting objects in the test image and visualizing the detections.
     """
-    holds_detector = ModelLoader(os.path.join(MODELS_DIRECTORY, "experiment_config.yml"), os.path.join(MODELS_DIRECTORY, "model_final.pth"))
-    skeleton_detector = ModelLoader(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"), model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
-    
-    video = cv2.VideoCapture(2)
-    if (video.isOpened() == False): 
+    holds_detector = ModelLoader(os.path.join(MODELS_DIRECTORY, "holds_model_yolov8l.pt"))
+    skeleton_detector = ModelLoader("yolov8l-pose.pt")
+
+    video = cv2.VideoCapture(os.path.join(VIDEOS_DIRECTORY, "Escalade_Mouvement.mp4"))
+    video.set(cv2.CAP_PROP_FRAME_WIDTH, video.get(cv2.CAP_PROP_FRAME_WIDTH)/2)
+    video.set(cv2.CAP_PROP_FRAME_HEIGHT, video.get(cv2.CAP_PROP_FRAME_WIDTH)/2)
+
+    if (video.isOpened() == False):
         print("Error opening video stream or file")
 
-    scanner = SkeletonScanner(2)
-
     success, image = video.read()
-    refresh_indicator = None
-    should_refresh = True
     while success:
         success, image = video.read()
         if not success:
             break
 
-        if refresh_indicator is not "no-refresh" and refresh(image, refresh_indicator, 10, 10):
-            refresh_indicator = image[10, 10] if should_refresh else "no-refresh"
-            outputs = holds_detector.predict(image)
-            boxes = convert_image_box_output(outputs)
+        holds_predictions = holds_detector.predict(image)
+        holds_boxes = convert_image_box_outputs(holds_predictions)
+        image = box_visualizer(image, holds_boxes, color=(0, 255, 0), thickness=1)
 
-        image = box_visualizer(image, boxes, color=(0, 255, 0), thickness=1)
-        skeleton = scanner.generateSkeleton(image)[1]
-        image = skeleton_visualizer(image, skeleton, color=(0, 0, 255), thickness=1)
+        skeleton_prediction = skeleton_detector.predict(image)
+        skeletons = convert_image_skeleton_outputs(skeleton_prediction)
 
-        if isinstance(skeleton, Skeleton):
-            members_to_check = ["main_1", "main_2", "pied_1", "pied_2"]
-            for box in boxes:
-                for member in skeleton.body.keys():
-                    if member in members_to_check:
-                        member_position = skeleton.body[member]
-                        if box.position_collide(member_position, margin=10):
-                            cv2.rectangle(image, (int(box.positions[0].x), int(box.positions[0].y)), (int(box.positions[1].x), int(box.positions[1].y)), (0, 0, 255), 2)
-                            cv2.circle(image, (int(member_position.x), int(member_position.y)), 5, (0, 255, 0), 2)
-                            members_to_check.remove(member)
-                            break
+        for skeleton in skeletons:
+            image = skeleton_visualizer(image, skeleton, color=(0, 0, 255), thickness=1)
+
+            if isinstance(skeleton, Skeleton):
+                members_to_check = ["main_1", "main_2", "pied_1", "pied_2"]
+                for hold_box in holds_boxes:
+                    for member in skeleton.body.keys():
+                        if member in members_to_check:
+                            member_position = skeleton.body[member]
+                            if hold_box.position_collide(member_position, margin=10):
+                                cv2.rectangle(image, (int(hold_box.positions[0].x), int(hold_box.positions[0].y)), (int(hold_box.positions[1].x), int(hold_box.positions[1].y)), (0, 0, 255), 2)
+                                cv2.circle(image, (int(member_position.x), int(member_position.y)), 5, (0, 255, 0), 1)
+                                members_to_check.remove(member)
+                                break
 
         cv2.imshow("Holds", image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 
-def test_skeleton_scanner_file():
-    """
-    Tests the SkeletonScanner class by generating a skeleton from the test image and visualizing the skeleton.
-    """
-    skeleton_scanner = SkeletonScanner(os.path.join(VIDEOS_DIRECTORY, "Escalade_Prof.mp4"), frequency=4)
-    skeleton_scanner.generateParcours()
-
-
-def test_skeleton_scanner_webcam():
-    """
-    Tests the SkeletonScanner class by generating a skeleton from the webcam and visualizing the skeleton.
-    """
-    skeleton_scanner = SkeletonScanner(0)
-    skeleton_scanner.generateParcours()
-
-
 if __name__ == "__main__":
-    cv2.namedWindow("Holds")
-    cv2.setMouseCallback("Holds", mouse_callback)
+    # cv2.namedWindow("Holds")
+    # cv2.setMouseCallback("Holds", mouse_callback)
     test_holds_detector()
-    # test_skeleton_scanner_file()
-    # test_skeleton_scanner_webcam()
