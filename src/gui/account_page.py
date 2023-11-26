ce4318972771863be3a533f0bb3380c50f070017
Djingoo <56677342+Djingoo@users.noreply.github.com>
2023-11-26 12:24:37 +0100

    added cam + framerate parameters
    - Cam parameters unfinished due to error
    - Framerate unfinished due to not knowing where it's stored

--- src/gui/account_page.py
+++ src/gui/account_page.py
@@ -3,8 +3,11 @@ import tkinter as tk
 import customtkinter
 from gui.abstract.page import Page
 from gui import login_page
+from utils.camera_discover_utils import get_available_cameras_names
+from gui.app_state import AppState
 
-from gui.utils import SECONDARY_COLOR, SECONDARY_HOVER_COLOR
+from gui.utils import SECONDARY_COLOR, SECONDARY_HOVER_COLOR, FONT
+from gui.utils import v, UV, IUV, min_max_range
 
 class account_page(Page):
     """Class of the account page."""
@@ -19,11 +22,42 @@ class account_page(Page):
         self.grid_rowconfigure((0,1,2,3,4), weight=1)
 
         self.account_label = customtkinter.CTkLabel(self, text="Account", font=("Arial", 30))
-        self.account_label.grid(row=0, column=0, columnspan=2)
+        self.account_label.grid(row=0, column=0)
+
+        self.choose_cam_label = customtkinter.CTkLabel(self, text="Choose camera", font=("Arial", 20))
+        self.choose_cam_label.grid(row=1, column=1, sticky="e")
+
+        cam_list = get_available_cameras_names()
+        self.choose_cam_combobox = customtkinter.CTkComboBox(self, values=cam_list, state="readonly")
+        self.choose_cam_combobox.grid(row=1, column=2,padx=10, sticky="w")
+
+        self.frame_rate_label = customtkinter.CTkLabel(self, text="Frame rate", font=("Arial", 20))
+        self.frame_rate_label.grid(row=2, column=1, sticky="e")
+
+        self.frame_rate_combobox = customtkinter.CTkComboBox(self, values=["30", "60"], state="readonly")
+        self.frame_rate_combobox.grid(row=2, column=2,padx=10, sticky="w")
+
+        self.choose_cam_button = customtkinter.CTkButton(self, text="Validate", fg_color=SECONDARY_COLOR, hover_color=SECONDARY_HOVER_COLOR, command=self.validate)
+        self.choose_cam_button.grid(row=3, column=1, columnspan=2)
 
         self.desconnect_button = customtkinter.CTkButton(self, text="Log out", fg_color=SECONDARY_COLOR, hover_color=SECONDARY_HOVER_COLOR, command=self.desconnect)
-        self.desconnect_button.grid(row=4, column=4)
+        self.desconnect_button.grid(row=4, column=4,padx=10)
 
+    def validate(self):
+        """Validate the settings."""
+        self.choose_cam()
+        self.choose_frame_rate()
+
+    def choose_cam(self):
+        """Choose the camera."""
+        # AppState.set_camera_name(self, self.choose_cam_combobox.get())
+        # print("Camera set to", self.choose_cam_combobox.get())
+        pass
+
+    def choose_frame_rate(self):
+        """Choose the frame rate."""
+        # print("Frame rate set to", self.frame_rate_combobox.get())
+        pass
 
     def desconnect(self):
         """Desconnect the user."""
