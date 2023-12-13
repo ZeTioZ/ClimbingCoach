de8aef3bf7f1e7aa37f5875c4633f540a820e033
ZeTioZ <donato_gentile@hotmail.it>
2023-12-13 21:11:11 +0100

    Fixed route model foreign key missing
    Took 5 minutes

--- src/database/models/route.py
+++ src/database/models/route.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Integer, String, LargeBinary
+from sqlalchemy import Integer, String, LargeBinary, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base_table import BaseTable
+from database.models.wall import Wall
 
 
 class Route(BaseTable):
@@ -12,3 +13,4 @@ class Route(BaseTable):
 	difficulty: Mapped[Integer] = mapped_column(Integer, nullable=False)
 	image: Mapped[LargeBinary] = mapped_column(LargeBinary, nullable=False)
 	holds: Mapped[LargeBinary] = mapped_column(LargeBinary, nullable=False)
+	wall_name: Mapped[Wall] = mapped_column(String, ForeignKey('wall.name', ondelete='CASCADE'), nullable=False)
